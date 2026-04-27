30325c86548a9a6bc102098a53d2e4d465d5a30d
CristianT2 <c.torrejon96@gmail.com>
2026-04-27 00:19:05 -0300

    Se diseño la pagina Blog del sitio web

--- js/scripts.js
+++ js/scripts.js
@@ -188,4 +188,37 @@ $(document).ready(function () {
             $('#preciosTabla td, #preciosTabla th').removeClass('columna-activa');
         });
     });
+
+    //Funciones para la pagina de Blog
+    $(document).ready(function() {
+    // 1. EFECTO APARECER AL BAJAR
+    function revealPosts() {
+        $('.post').each(function() {
+            let pos = $(this).offset().top;
+            let win = $(window).scrollTop() + $(window).height() - 100;
+            if (win > pos) {
+                $(this).css({'opacity': '1', 'transform': 'translateY(0)', 'transition': '0.6s all ease-out'});
+            }
+        });
+    }
+
+    $(window).scroll(revealPosts);
+    revealPosts(); // Para los que ya están arriba
+
+    // 2. FILTRADO JQUERY
+    $('.btn-filter').click(function() {
+        $('.btn-filter').removeClass('active');
+        $(this).addClass('active');
+        
+        let cat = $(this).attr('data-filter');
+        
+        $('.post').hide();
+        if(cat === 'all') {
+            $('.post').fadeIn(400);
+        } else {
+            $('.post.' + cat).fadeIn(400);
+        }
+        revealPosts();
+    });
+});
 });
